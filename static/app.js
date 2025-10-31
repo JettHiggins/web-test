95aaec768a0aa404011bedd9b90b0fc4c2414062
Jett Higgins <jhiggins2@stevens.edu>
2025-10-31 13:47:05 -0400

    Slight cleanup

--- static/app.js
+++ static/app.js
@@ -1,9 +1,4 @@
 
-// Plan, Have a Text Box That can take in copied Text and Images, 
-// Have a button to send the copied Text/image to the backend
-// Store the Text/Image in the database of whoever is using it.
-// Need to add a rich text editor to website.
-
 const quill = new Quill('#editor', {
   modules: { toolbar: false },
   theme: 'snow',
