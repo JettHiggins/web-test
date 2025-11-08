8f0c997d467dc90057dcb25f127469403e966d8e
Jett Higgins <jhiggins2@stevens.edu>
2025-11-08 14:11:09 -0500

    Initial Popup added

--- static/app.js
+++ static/app.js
@@ -9,3 +9,21 @@ let getInput = function() {
   const contents = quill.getContents();
   console.log(contents);
 }
+
+let userToggle = () => {
+  if (document.querySelector('.popup') == null){
+    showPopup()
+  }
+  else{
+    document.querySelector('.popup').remove()
+  }
+  
+}
+
+let showPopup = () => {
+  const popup = document.createElement('div')
+  const userInfo = document.querySelector('.login-text')
+  popup.classList.add("popup")
+  userInfo.append(popup)
+
+}
